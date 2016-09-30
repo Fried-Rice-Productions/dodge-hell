d65d6da0d84d64eb23a5f5d7ee0f06f2fe315f39
JoeCarboxyl <josephjkoo@gmail.com>
2016-09-29 23:24:48 -0500

    boundary fixed

--- Assets/InProgress/Scripts/PlayerMovement.cs
+++ Assets/InProgress/Scripts/PlayerMovement.cs
@@ -20,24 +20,46 @@ public class PlayerMovement : MonoBehaviour {
 		rb = GetComponent<Rigidbody2D> ();
 	}
 	public Boundary boundary;
+
+	void Update
+	{
+		Instantiate 
+	}
 	// Update is called once per frame
 	void FixedUpdate ()
 	{
+		
 	/*	rb.position = new Vector3 (
 			Mathf.Clamp (rb.position.x, boundary.xMin, boundary.xMax),
-			Mathf.Clamp (rb.position.y, boundary.yMin, boundary.yMax)
+			Mathf.Clamp (rb.position.y, boundary.xMin, boundary.xMax)
 		);*/
 		if (Input.GetKey (KeyCode.RightArrow)) {
 			transform.Translate (speed * Vector3.right * Time.deltaTime);
+			Vector3 clampedPosition = transform.position;
+			clampedPosition.x = Mathf.Clamp(transform.position.x, boundary.xMin, boundary.xMax);
+			clampedPosition.y = Mathf.Clamp(transform.position.y, boundary.yMin, boundary.yMax);
+			transform.position = clampedPosition;
 		}
 		if (Input.GetKey (KeyCode.LeftArrow)) {
 			transform.Translate (speed * Vector3.left * Time.deltaTime);
+			Vector3 clampedPosition = transform.position;
+			clampedPosition.x = Mathf.Clamp(transform.position.x, boundary.xMin, boundary.xMax);
+			clampedPosition.y = Mathf.Clamp(transform.position.y, boundary.yMin, boundary.yMax);
+			transform.position = clampedPosition;
 		}
 		if (Input.GetKey (KeyCode.UpArrow)) {
 			transform.Translate (speed * Vector3.up * Time.deltaTime);
+			Vector3 clampedPosition = transform.position;
+			clampedPosition.x = Mathf.Clamp(transform.position.x, boundary.xMin, boundary.xMax);
+			clampedPosition.y = Mathf.Clamp(transform.position.y, boundary.yMin, boundary.yMax);
+			transform.position = clampedPosition;
 		}
 		if (Input.GetKey (KeyCode.DownArrow)) {
 			transform.Translate (speed * Vector3.down * Time.deltaTime);
+			Vector3 clampedPosition = transform.position;
+			clampedPosition.x = Mathf.Clamp(transform.position.x, boundary.xMin, boundary.xMax);
+			clampedPosition.y = Mathf.Clamp(transform.position.y, boundary.yMin, boundary.yMax);
+			transform.position = clampedPosition;
 		}
 				//bounce back result of movement method conflicting with boxcollider boundaries
 	}
