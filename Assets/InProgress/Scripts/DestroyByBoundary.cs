902d540f1091e1fda7ac765ed17c02e6076da7ee
ebenarnold <eben1996@gmail.com>
2016-10-08 19:17:15 -0500

    made the players shoot basketballs

---
+++ Assets/InProgress/Scripts/DestroyByBoundary.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByBoundary : MonoBehaviour {
+
+	void OnTriggerExit2D(Collider2D other){
+		Destroy (other.gameObject);
+	}
+}
--- Assets/InProgress/Scripts/shotBehavior.cs
+++ Assets/InProgress/Scripts/shotBehavior.cs
@@ -4,17 +4,22 @@ using System.Collections;
 public class shotBehavior : MonoBehaviour {
 	
 	public float speed;
-	private Rigidbody2D rb;
 
 	// Use this for initialization
 	void Start () {
-		//gave it some SPEED
-		rb = GetComponent<Rigidbody2D>();
-		rb.velocity = transform.forward * speed;
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		//get shot current pos
+		Vector2 pos = transform.position;
+		 //compute new pos
+		pos = new Vector2(pos.x + speed * Time.deltaTime , pos.y);
+		//update current pos
+		transform.position = pos;
+
+		//destroy when leaves background
 
 	}
 }
