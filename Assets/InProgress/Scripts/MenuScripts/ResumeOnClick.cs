a7b76f75dfa29c6857c07c601d11054d47b52f56
John Pham <newhope11134@gmail.com>
2016-10-08 19:19:31 -0500

    Start of pause functionality

---
+++ Assets/InProgress/Scripts/MenuScripts/ResumeOnClick.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResumeOnClick : MonoBehaviour {
+
+	public PauseOnClick pauseOnClick;
+
+	public void resume() {
+		pauseOnClick.isPaused = false;
+		pauseOnClick.handleIsPaused ();
+	}
+}
