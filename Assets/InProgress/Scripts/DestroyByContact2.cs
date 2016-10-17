0113af5c42b627316d6fb0204026a3225f3b5fc9
ebenarnold <eben1996@gmail.com>
2016-10-16 21:31:14 -0500

    bullets destroy git add --a

---
+++ Assets/InProgress/Scripts/DestroyByContact2.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByContact2 : MonoBehaviour {
+	//public GameObject explosion;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.CompareTag ("Boundary") || other.CompareTag("Player2") ) {
+			return;
+		}
+
+		Destroy (other.gameObject);
+		Destroy (gameObject);
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
